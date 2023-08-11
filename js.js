6f4f130a107948f10821b00caeb5f00ee1373686
M.M Allahyari <36984791+mm706@users.noreply.github.com>
2023-08-11 13:48:17 +0330

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,50 @@
-let counter = 1;
-setInterval( function (){
-    document.getElementById('radio' + counter).checked = true;
-    counter ++;
-    if (counter > 5){
-        counter = 1;
-    }
-}, 3000);
+const bullets = document.querySelectorAll('.bullet');
+const imgs = document.querySelectorAll('.img');
+// const firstImg = document.getElementsByClassName('.first');
+let currentImg = 1;
+let manualNav = function (manual){
+    imgs.forEach((img)=>{
+       img.classList.remove('active');
+
+       bullets.forEach((bullet)=>{
+           bullet.classList.remove('active');
+       });
+    });
+    bullets[manual].classList.add('active');
+    imgs[manual].classList.add('active');
+}
+
+bullets.forEach((bullet , i) =>{
+        bullet.addEventListener('click', () =>{
+            manualNav(i);
+            currentImg = i;
+
+        });
+});
+
+
+let repeat = function (activeClass){
+  let active = document.getElementsByClassName('active');
+  let i = 1;
+  let repeater = () => {
+      setTimeout(function (){
+          [...active].forEach((activeImg) =>{
+              activeImg.classList.remove('active')
+          });
+         imgs[i].classList.add('active');
+         bullets[i].classList.add('active');
+         i++;
+         if (imgs.length == i){
+             i = 0;
+         }
+         if(i >= imgs.length){
+             return;
+         }
+         repeater();
+      } ,5000);
+  }
+    repeater();
+};
+repeat();
+
+
